defa64a7561a65725d87a53e0ee5155f5ac4e6c1
Quocdung2030 <57708444+Quocdung2030@users.noreply.github.com>
2019-11-13 17:32:12 +0700

    Add files via upload

---
+++ bannerdautrang-rvptw.js
@@ -0,0 +1,147 @@
+//please contact me vanmanh2030@gmail.com
+//website locgia.vn or thegioinhadatchinhchu.com
+
+(function($) {
+    $.fn.linhnguyen = function(options) {        
+        var defaults = {  
+	    	animation: 'fadeAndPop', //fade, fadeAndPop, none
+		    animationspeed: 300, //how fast animtions are
+		    closeonbackgroundclick: true, //if you click background will modal close?
+		    dismissmodalclass: 'close-linhnguyen-modal' //the class of a button or element that will close an open modal
+    	}; 
+
+        var options = $.extend({}, defaults, options); 
+	
+        return this.each(function() {
+        	var modal = $(this),
+        		topMeasure  = parseInt(modal.css('top')),
+				topOffset = modal.height() + topMeasure,
+          		locked = false,
+				modalBG = $('.linhnguyen-modal-bg');
+
+			if(modalBG.length == 0) {
+				modalBG = $('<div class="linhnguyen-modal-bg" />').insertAfter(modal);
+			}		    
+			modal.bind('linhnguyen:open', function () {
+			  modalBG.unbind('click.modalEvent');
+				$('.' + options.dismissmodalclass).unbind('click.modalEvent');
+				if(!locked) {
+					lockModal();
+					if(options.animation == "fadeAndPop") {
+						modal.css({'top': $(document).scrollTop()-topOffset, 'opacity' : 0, 'visibility' : 'visible'});
+						modalBG.fadeIn(options.animationspeed/2);
+						modal.delay(options.animationspeed/2).animate({
+							"top": $(document).scrollTop()+topMeasure + 'px',
+							"opacity" : 1
+						}, options.animationspeed,unlockModal());					
+					}
+					if(options.animation == "fade") {
+						modal.css({'opacity' : 0, 'visibility' : 'visible', 'top': $(document).scrollTop()+topMeasure});
+						modalBG.fadeIn(options.animationspeed/2);
+						modal.delay(options.animationspeed/2).animate({
+							"opacity" : 1
+						}, options.animationspeed,unlockModal());					
+					} 
+					if(options.animation == "none") {
+						modal.css({'visibility' : 'visible', 'top':$(document).scrollTop()+topMeasure});
+						modalBG.css({"display":"block"});	
+						unlockModal()				
+					}
+				}
+				modal.unbind('linhnguyen:open');
+			}); 	
+
+			modal.bind('linhnguyen:close', function () {
+			  if(!locked) {
+					lockModal();
+					if(options.animation == "fadeAndPop") {
+						modalBG.delay(options.animationspeed).fadeOut(options.animationspeed);
+						modal.animate({
+							"top":  $(document).scrollTop()-topOffset + 'px',
+							"opacity" : 0
+						}, options.animationspeed/2, function() {
+							modal.css({'top':topMeasure, 'opacity' : 1, 'visibility' : 'hidden'});
+							unlockModal();
+						});					
+					}  	
+					if(options.animation == "fade") {
+						modalBG.delay(options.animationspeed).fadeOut(options.animationspeed);
+						modal.animate({
+							"opacity" : 0
+						}, options.animationspeed, function() {
+							modal.css({'opacity' : 1, 'visibility' : 'hidden', 'top' : topMeasure});
+							unlockModal();
+						});					
+					}  	
+					if(options.animation == "none") {
+						modal.css({'visibility' : 'hidden', 'top' : topMeasure});
+						modalBG.css({'display' : 'none'});	
+					}		
+				}
+				modal.unbind('linhnguyen:close');
+			});     
+   	
+    	modal.trigger('linhnguyen:open')
+			
+			//Close Modal Listeners
+			var closeButton = $('.' + options.dismissmodalclass).bind('click.modalEvent', function () {
+			  modal.trigger('linhnguyen:close')
+			});
+			
+			if(options.closeonbackgroundclick) {
+				modalBG.css({"cursor":"pointer"})
+				modalBG.bind('click.modalEvent', function () {
+				  modal.trigger('linhnguyen:close')
+				});
+			}
+			$('body').keyup(function(e) {
+        		if(e.which===27){ modal.trigger('linhnguyen:close'); } // 27 is the keycode for the Escape key
+			});
+			
+			function unlockModal() { 
+				locked = false;
+			}
+			function lockModal() {
+				locked = true;
+			}	
+			
+        });//each call
+    }//orbit plugin call
+})(jQuery);
+        
+$(window).load(function() {
+		if(document.cookie.indexOf("adf") == -1)
+		{
+			document.cookie = "adfpopunder1=adf";
+			var divpopup = document.createElement("div");
+			$(divpopup).attr('id', 'myModal');
+			$(divpopup).attr('class', 'linhnguyen-modal');
+			$( "body" ).append(divpopup);
+			$("#myModal").html("<div class='f_dangkytuvan'>
+
+        	<div class='fdangkytuvan_center'>
+
+                <div class='fdangkytuvan_cl'>
+
+                    <p class='dktv_style1'>Chào mừng đã ghé thăm website của chúng tôi</p>
+
+                    <p class='dktv_style2'>Đăng ký tư vấn miễn phí <span style='color:#FC0; font-weight:600;'>0909 065 423</span></p>
+
+                </div>
+
+                
+
+               <form action='https://docs.google.com/forms/u/0/d/e/1FAIpQLSe1ogsz3vtHI_m-srOq_udF3bFFYJP231ag4r2p7ONDbtlEWQ/formResponse?embedded=true' class='fdangkytuvan_cl' id='form_dathang' method='POST' onsubmit='return emptycart()' target='hidden_iframe'>
+
+                        <input class='input_dktuvan' id='entry_1253501127' name='entry.1253501127' placeholder='Họ Tên' required='' type='text' value=''/>
+
+                        <input class='input_dktuvan' id='entry_1396539562' name='entry.1396539562' placeholder='Điện thoại' required='' type='number' value=''/>
+
+                        <input class='input_dktuvan' id='entry_1837020261' name='entry.1837020261' placeholder='Yêu Cầu Của Quý Khách' required='' type='text' value=''/>
+<input class='btdangkytuvan' id='btdktuvan1' name='submit' type='submit' value='Gửi'/>
+                        
+
+  </form>");
+			$('#myModal').linhnguyen($('#myModal').data());
+		}
+});
